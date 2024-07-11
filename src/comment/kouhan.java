18aa4023314512fa0166d68f863714d0e384733b
yuhei.nishimura.2005 <yuhei.nishimura,2005@gmail.com>
2024-07-11 16:15:23 +0900

    1.0

---
+++ src/comment/kouhan.java
@@ -0,0 +1,15 @@
+package comment;
+
+public class kouhan {
+	public static void callDeae() {
+		System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
+	}
+
+	public static void showMondokoro() throws Exception {
+		System.out.println("飛車さん、角さん。もういいでしょう。");
+		System.out.println("この紋所が目にはいらぬか！");
+		Thread.sleep(3000);
+		zenhan.doTogame(); // もう一度、とがめる
+	}
+
+}
